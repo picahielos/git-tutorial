0d8bd808a775aaec67a72feb5bb7b6dbdcb9b725
toni.lopez <toni.lopez@shazam.com>
2015-06-06 11:20:27 +0100

    Adding comments.

--- JavaTutorial/src/javatutorial/JavaTutorial.java
+++ JavaTutorial/src/javatutorial/JavaTutorial.java
@@ -1,7 +1,12 @@
 package javatutorial;
 
 public class JavaTutorial {
+    /**
+     * This is the first method that will be executed.
+     * @param args 
+     */
     public static void main(String[] args) {
+        // this is a comment
         System.out.println("Hola Neri!");
     }
 }
